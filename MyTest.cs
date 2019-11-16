68498cf1f440db8694474f1b55a5efa31be2ae41
Vasily Kabanov <vasily.kabanov@gmail.com>
2019-11-16 22:12:34 +1100

    moved .Select() test back to original test

--- MyTest.cs
+++ MyTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Data.SQLite;
@@ -8,16 +10,35 @@ using NUnit.Framework;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
+using NHibernate.Hql.Ast.ANTLR;
 using NHibernate.Linq;
 using NHibernate.Tool.hbm2ddl;
 
 namespace Nh526QueryBug
 {
+    public static class ConnectionExtensions
+    {
+        public static int ExecuteNonQuery(this SqlConnection cn, string query)
+        {
+            if (cn.State != ConnectionState.Open)
+                cn.Open();
+
+            using (var cm = cn.CreateCommand())
+            {
+                cm.CommandText = query;
+                cm.CommandType = CommandType.Text;
+
+                return cm.ExecuteNonQuery();
+            }
+        }
+    }
+
     [TestFixture]
     public class MyTest
     {
-
         const string ConnectionStringInMemory = "Data Source=:memory:;Version=3;New=True;";
+        const string ConnectionStringMsSqlSetup = "Server=.; Database=master; Integrated Security=yes;";
+        const string MsSqlDatabaseName = "NhTest";
         const string ConnectionStringMsSql = "Server=.; Database=NhTest; Integrated Security=yes;";
 
         const string Mapping = @"<hibernate-mapping assembly=""Nh526QueryBug"" namespace=""Nh526QueryBug"" xmlns=""urn:nhibernate-mapping-2.2"">
@@ -54,10 +75,78 @@ namespace Nh526QueryBug
         <many-to-one name=""Parent"" class=""ChildEntity"" column=""ParentId"" />
     </class>
 
+    <class name=""Material"">
+        <id name=""Id"" type=""Int32"">
+            <generator class=""assigned""/>
+        </id>
+        <version name=""IntegrityVersion"" column=""IntegrityVersion"" unsaved-value=""0"" />
+
+        <many-to-one name=""MaterialDefinition"" class=""MaterialDefinition"" column=""MaterialDefinitionId"" not-null=""true"" not-found=""exception"" />
+        <many-to-one name=""ProductDefinition"" class=""ProductDefinition"" column=""ProductDefinitionId"" not-null=""true"" not-found=""exception"" />
+        
+        <property name=""Name"" type=""string"" />
+    </class>
+
+    <class name=""MaterialDefinition"">
+        <id name=""Id"" type=""Int32"">
+            <generator class=""assigned""/>
+        </id>
+        <version name=""IntegrityVersion"" column=""IntegrityVersion"" unsaved-value=""0"" />
+
+        <property name=""Name"" type=""string"" />
+    </class>
+
+    <class name=""ProductDefinition"">
+        <id name=""Id"" type=""Int32"">
+            <generator class=""assigned""/>
+        </id>
+        <version name=""IntegrityVersion"" column=""IntegrityVersion"" unsaved-value=""0"" />
+        <many-to-one name=""MaterialDefinition"" class=""MaterialDefinition"" column=""MaterialDefinitionId"" not-null=""true"" not-found=""exception"" />
+
+        <property name=""Name"" type=""string"" />
+    </class>
+
 </hibernate-mapping>";
 
-        [Test]
-        public void TestSqLite()
+        private ISessionFactory SessionFactorySqLite { get; set; }
+
+        private ISessionFactory _sessionFactoryMs;
+
+        private ISessionFactory SessionFactoryMs
+        {
+            get
+            {
+                if (_sessionFactoryMs == null)
+                {
+                    var configuration = new Configuration();
+                    configuration
+                        .DataBaseIntegration(
+                            x =>
+                            {
+                                x.ConnectionString = ConnectionStringMsSql;
+                                x.Driver<SqlClientDriver>();
+                                x.Dialect<MsSql2012Dialect>();
+                                x.LogSqlInConsole = true;
+                                x.LogFormattedSql = true;
+                            })
+                        .AddXmlString(Mapping);
+                    configuration.BuildMappings();
+
+                    OneTimeEnsureTestSqlDatabaseExists(configuration);
+
+                    _sessionFactoryMs = configuration.BuildSessionFactory();
+                }
+
+                return _sessionFactoryMs;
+            }
+        }
+
+        private ISession OpenSqLiteSession() => OpenSession(new SQLiteConnection(ConnectionStringInMemory), SessionFactorySqLite);
+
+        private ISession OpenMsSqlSession() => OpenSession(new SqlConnection(ConnectionStringMsSql), SessionFactoryMs);
+
+        [OneTimeSetUp]
+        public void SetUp()
         {
             var configuration = new Configuration();
             configuration
@@ -73,71 +162,84 @@ namespace Nh526QueryBug
                 .AddXmlString(Mapping);
             configuration.BuildMappings();
 
+            SessionFactorySqLite = configuration.BuildSessionFactory();
+
             using (var connection = new SQLiteConnection(ConnectionStringInMemory))
             {
                 connection.Open();
 
-                SetUpAndRunTestQuery(configuration, connection);
+                new SchemaExport(configuration).Execute(false, true, false, connection, TestContext.Progress);
+            }
+        }
+
+        [Test]
+        public void TestSelectManySqLite()
+        {
+            using (var session = OpenSqLiteSession())
+            {
+                TestQueryInternal(session);
+            }
+        }
+
+        [Test]
+        public void TestSelect()
+        {
+            using (var session = OpenSqLiteSession())
+            {
+                var selectedProductDefinition = new ProductDefinition() { Id = 1000, MaterialDefinition = new MaterialDefinition { Id = 1 } };
+                session.Save(selectedProductDefinition.MaterialDefinition);
+                session.Save(selectedProductDefinition);
+
+                var selectedProducts = new[] { selectedProductDefinition };
+
+                var query = session.Query<Material>()
+                    .Where(x => selectedProducts.Contains(x.ProductDefinition) && selectedProducts.Select(y => y.MaterialDefinition).Contains(x.MaterialDefinition));
+
+                var sessionImpl = session.GetSessionImplementation();
+                var factoryImplementor = sessionImpl.Factory;
+
+                var nhLinqExpression = new NhLinqExpression(query.Expression, factoryImplementor);
+                var translatorFactory = new ASTQueryTranslatorFactory();
+                var translator = translatorFactory.CreateQueryTranslators(nhLinqExpression, null, false, sessionImpl.EnabledFilters, factoryImplementor).First();
+
+                TestContext.WriteLine(translator.SQLString);
+
+                Assert.IsNotEmpty(nhLinqExpression.ParameterValuesByName.Values.Where(x => (x.Item1 as IList<ProductDefinition>)?.Contains(selectedProductDefinition) == true));
+                Assert.IsNotEmpty(nhLinqExpression.ParameterValuesByName.Values.Where(x => (x.Item1 as IList<MaterialDefinition>)?.Contains(selectedProductDefinition.MaterialDefinition) == true));
+
+                //var result = query.ToList();
             }
         }
 
         [Test]
         public void TestMsSql()
         {
-            var configuration = new Configuration();
-            configuration
-                .DataBaseIntegration(
-                    x =>
-                    {
-                        x.ConnectionString = ConnectionStringMsSql;
-                        x.Driver<SqlClientDriver>();
-                        x.Dialect<MsSql2012Dialect>();
-                        x.LogSqlInConsole = true;
-                        x.LogFormattedSql = true;
-                    })
-                .AddXmlString(Mapping);
-            configuration.BuildMappings();
-
-            EnsureTestSqlDatabaseExists();
-
-            using (var connection = new SqlConnection(ConnectionStringMsSql))
-            {
-                connection.Open();
-
-                SetUpAndRunTestQuery(configuration, connection);
-            }
-        }
-
-        private void SetUpAndRunTestQuery(Configuration configuration, DbConnection connection)
-        {
-            var sessionFactory = configuration.BuildSessionFactory();
-
-            new SchemaExport(configuration).Execute(false, true, false, connection, TestContext.Progress);
-            using (var session = sessionFactory.WithOptions().Connection(connection).OpenSession())
-            {
-                var parent = CreateTestParentEntity();
-                session.Save(parent);
-                session.Flush();
-            }
-
-            using (var session = sessionFactory.WithOptions().Connection(connection).OpenSession())
+            using (var session = OpenMsSqlSession())
             {
                 TestQueryInternal(session);
             }
         }
 
-        private void EnsureTestSqlDatabaseExists()
+        private ISession OpenSession(DbConnection connection, ISessionFactory factory)
         {
-            var csBuilder = new SqlConnectionStringBuilder(ConnectionStringMsSql);
-            var targetDatabaseName = csBuilder.InitialCatalog;
-            csBuilder.InitialCatalog = "master";
-            using (var connection = new SqlConnection(csBuilder.ConnectionString))
-            {
+            if (connection.State != ConnectionState.Open)
                 connection.Open();
+            var session = factory.WithOptions().Connection(connection).OpenSession();
+            return session;
+        }
 
-                var sqlCommand = connection.CreateCommand();
-                sqlCommand.CommandText = $"if (db_id('{targetDatabaseName}')) is null create database [{targetDatabaseName}]";
-                sqlCommand.ExecuteNonQuery();
+        private void OneTimeEnsureTestSqlDatabaseExists(Configuration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+
+            using (var cn = new SqlConnection(ConnectionStringMsSqlSetup))
+            {
+                cn.Open();
+                cn.ExecuteNonQuery($@"if db_id(N'{MsSqlDatabaseName}') is not null drop database [{MsSqlDatabaseName}]; create database [{MsSqlDatabaseName}];");
+
+                cn.ChangeDatabase(MsSqlDatabaseName);
+
+                new SchemaExport(configuration).Execute(false, true, false, cn, TestContext.Progress);
             }
         }
 
@@ -213,5 +315,26 @@ namespace Nh526QueryBug
     public class GrandChildEntity : Entity
     {
         public virtual ChildEntity Parent { get; set; }
+    }
+    public class EntityWithName : Entity
+    {
+        public virtual string Name { get; set; }
+    }
+
+    public class Material : EntityWithName
+    {
+
+        public virtual MaterialDefinition MaterialDefinition { get; set; }
+
+        public virtual ProductDefinition ProductDefinition { get; set; }
+    }
+
+    public class MaterialDefinition : EntityWithName
+    {
+    }
+
+    public class ProductDefinition : EntityWithName
+    {
+        public virtual MaterialDefinition MaterialDefinition { get; set; }
     }
 }
